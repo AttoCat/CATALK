a7687de56724d989f96ff021e571eb68294862af
AttoCat <wazasiyo2525a@gmail.com>
2020-04-29 14:23:32 +0900

    起動メッセ変更

--- discordbot.py
+++ discordbot.py
@@ -4,8 +4,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("on_ready 正常に起動しました")
+    print("on_ready 正常に起動しました No error occurred")
     print(discord.__version__)
 
 client.run("NzA0NjE5NzU0MTA2NzgxNzQ5.XqfyeA.4tGoPySZyW7TL3jSoN5cdIPiJuQ")
-# コメントテスト　成功できて嬉しい
