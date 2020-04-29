040aaf9b8500f3790613411db813c5f463e7afb0
AttoCat <wazasiyo2525a@gmail.com>
2020-04-29 14:33:49 +0900

    起動msg変更

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("on_ready 正常に起動しました No error occurred")
+    print("on_ready 正常に起動しました\n No error occurred")
     print(discord.__version__)
 
 client.run("NzA0NjE5NzU0MTA2NzgxNzQ5.XqfyeA.4tGoPySZyW7TL3jSoN5cdIPiJuQ")
