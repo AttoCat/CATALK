c9efd74b92d6161d21dac545506129d8c50d02a7
AttoCat <wazasiyo2525a@gmail.com>
2020-04-29 21:11:31 +0900

    トークンを見えなくする

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,11 @@
 import discord
+import dotenv
+dotenv.load_dotenv()
 client = discord.Client()
+TOKEN = os.getenv("TOKEN")
 
 
 @client.event
 async def on_ready():
     print("on_ready 正常に起動しました\n No error occurred")
     print(discord.__version__)
-
-client.run("NzA0NjE5NzU0MTA2NzgxNzQ5.XqfyeA.4tGoPySZyW7TL3jSoN5cdIPiJuQ")
