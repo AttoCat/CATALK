85a7ffc3c1cd60c184d9ec73a52244a03b49ef81
AttoCat <wazasiyo2525a@gmail.com>
2020-04-29 21:23:40 +0900

    cilentrun追加

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,10 @@ import os
 dotenv.load_dotenv()
 client = discord.Client()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+client.run(TOKEN)
 
 
 @client.event
 async def on_ready():
-    print("on_ready 正常に起動しました\n No error occurred")
+    print("on_ready")
     print(discord.__version__)
