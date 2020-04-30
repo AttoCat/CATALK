175182b518684758e8ca7862ae27dd8f52f42756
AttoCat <wazasiyo2525a@gmail.com>
2020-04-30 10:49:12 +0900

    cilent.run追加

--- discordbot.py
+++ discordbot.py
@@ -5,10 +5,11 @@ import os
 dotenv.load_dotenv()
 client = discord.Client()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-client.run(TOKEN)
 
 
 @client.event
 async def on_ready():
     print("on_ready")
     print(discord.__version__)
+
+client.run(TOKEN)
