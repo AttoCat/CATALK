e5cfc01389a3d38be4f4481894e6ce67f6e5a407
AttoCat <wazasiyo2525a@gmail.com>
2020-05-02 17:37:00 +0900

    起動時メッセージ追加

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ import os
 import discord
 import dotenv
 import datetime
+Start_ID = 704619077141921872
 
 dotenv.load_dotenv()
 client = discord.Client()
@@ -10,14 +11,22 @@ TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 
 @client.event
+async def greet():
+    channel = client.get_channel(Start_ID)
+    await channnel.send("正常に起動しました")
+
+
+@client.event
 async def on_ready():
     print("on_ready")
     print(discord.__version__)
+    await greet()
 
 
 @client.event
 async def on_message(message):
     if message.content == "おはよう！":
         await message.channel.send("おはようございますご主人さま" + "今日の日付は" + date.today() + "日です。")
+
 
 client.run(TOKEN)
