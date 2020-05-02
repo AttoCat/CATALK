d40098eefc0b80af7f786d7dd57bb0527e08f8a4
AttoCat <wazasiyo2525a@gmail.com>
2020-05-02 20:00:41 +0900

    indenterror修正

--- discordbot.py
+++ discordbot.py
@@ -7,10 +7,8 @@ dotenv.load_dotenv()
 client = discord.Client()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-
 Start_ID = 704619077141921872
 d_today = datetime.date.today()
-morningmessage = "おはようございますご主人さま。今日の日付は" + str(d_today) + "日です。"
 
 
 @client.event
@@ -29,6 +27,6 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == "おはよう！":
-        await message.channel.send(morningmessage)
+        await message.channel.send("おはようございますご主人さま。今日の日付は" + str(d_today) + "日です。")
 
 client.run(TOKEN)
