ccfe0d587ece051574c4fb2b0bb3ce884709d3da
AttoCat <wazasiyo2525a@gmail.com>
2020-05-06 13:25:10 +0900

    時刻応答テスト

--- discordbot.py
+++ discordbot.py
@@ -27,18 +27,25 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if (message.content == "おはよう！") or (message.content == "おはよう!"):
-        if message.channel.id == Start_ID:
+        if message.channel.id == Start_ID:  # 起動ログチャンネルで発言してもエラー&削除
             embed = discord.Embed(
                 title="Error",
                 description=(
-                    'ここでは実行できません！\nCannot perform this operation here.'),
+                    "ここでは実行できません！\nCannot perform this operation here."),
                 color=0xff0000)
             await client.get_channel(Start_ID).send(
                 embed=embed, delete_after=10)
             await message.delete()
             return
-        await message.channel.send(d_now.strftime(
-            f'おはようございます。\n'
-            f'今日の日付は%-m月%-d日です。'))
+        elif ((d_now.hour) > 11 and (d_now.hour) < 18):
+            await message.channel.send(d_now.strftime(
+                f"こんにちは。もう正午を過ぎています。「おはよう」と言うには遅い時間ですよ。\n"
+                f"今日の日付は%-m月%-d日です。"))
+            return
+        else:
+            await message.channel.send(d_now.strftime(
+                f"おはようございます。\n"
+                f"今日の日付は%-m月%-d日です。"))
+            return
 
 client.run(TOKEN)
