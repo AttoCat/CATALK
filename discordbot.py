c811e252bdc14885d3782d290d92b6f96aa88245
AttoCat <wazasiyo2525a@gmail.com>
2020-05-07 10:39:21 +0900

    formatテスト

--- discordbot.py
+++ discordbot.py
@@ -28,6 +28,8 @@ async def on_ready():
 async def on_message(message):
     if (message.content == "おはよう！") or (message.content == "おはよう!"):
         d_now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+        d_today = "今日の日付は" + \
+            d_now.strftime("%-m{0}%-d{1}").format(*"月日") + "です。"
         if message.channel.id == Start_ID:  # 起動ログチャンネルで発言してもエラー&削除
             embed = discord.Embed(
                 title="Error",
@@ -39,34 +41,17 @@ async def on_message(message):
             await message.delete()
             return
         elif 12 <= d_now.hour <= 19:
-            await message.channel.send(d_now.strftime(
-                f"こんにちは。もう正午を過ぎています。「おはよう」と言うには遅い時間ですよ。\n"
-                f"今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"こんにちは。もう正午を過ぎています。「おはよう」と言うには遅い時間ですよ。\n{d_today}"
         elif 20 <= d_now.hour <= 23:
-            await message.channel.send(d_now.strftime(
-                f"こんばんは。もう夜です。朝ではないこと、本当はあなたもわかっているんでしょう？\n"
-                f"今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"こんばんは。もう夜です。朝ではないこと、本当はあなたもわかっているんでしょう？\n{d_today}"
         elif d_now.hour == 24:
-            await message.channel.send(d_now.strftime(
-                f"こんばんは。もうおはようについてはスルーします。日付も変わったところですし早く寝ましょうよ。\n"
-                f"今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"こんばんは。もうおはようについてはスルーします。日付も変わったところですし早く寝ましょうよ。\n{d_today}"
         elif 24 < d_now.hour < 3:
-            await message.channel.send(d_now.strftime(
-                f"え…？まだ起きているんですか…？もう夜中ですよ。明らかにおはようじゃないでしょうが！\n"
-                f"とっくに日付も変わってますよ。今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"え…？まだ起きているんですか…？もう夜中ですよ。明らかにおはようじゃないでしょうが！\n{d_today}"
         elif 3 <= d_now.hour <= 5:
-            await message.channel.send(d_now.strftime(
-                f"おはようございます、なんでしょうか。まだ日は昇っていませんが。\n"
-                f"今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"おはようございます、なんでしょうか。まだ日は昇っていませんが。\n{d_today}"
         else:
-            await message.channel.send(d_now.strftime(
-                f"おはようございます。\n"
-                f"今日の日付は%-m月%-d日です。"))
-            return
+            msg = f"おはようございます。\n{d_today}"
+        await message.channel.send(msg)
 
 client.run(TOKEN)
