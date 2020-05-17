45ee7ef3917c2be74e726d8e439551c7bc4f486d
AttoCat <wazasiyo2525a@gmail.com>
2020-05-17 13:35:36 +0900

    指定ちゃんねる発言テスト

--- discordbot.py
+++ discordbot.py
@@ -60,8 +60,8 @@ async def ttset(message):
         title="時間割",
         description=f"明日の時間割\n{tt[0]}\n{tt[1]}\n{tt[2]}\n{tt[3]}\n{tt[4]}\n{tt[5]}\n{tt[6]}",
         color=0x0080ff)
-    channel = 711397925103599621
-    await channel.send(embed=embed)
+    await client.get_channel(TT_ID).send(
+        embed=embed, delete_after=60)
 
 
 @client.event
