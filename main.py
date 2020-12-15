38a4333d239c6fee26adbcf474b5b3284c32d05b
AttoCat <wazasiyo2525a@gmail.com>
2020-12-15 17:40:42 +0900

    微修正

--- main.py
+++ main.py
@@ -38,12 +38,17 @@ class Catalk(commands.Bot):
             except Exception:
                 traceback.print_exc()
 
+    async def on_ready(self):
+        print(f"Bot is ready! \nlibrary version:{discord.__version__}")
+        channel = bot.get_channel(706779308211044352)
+        await channel.send(f"{self.user}: 起動完了")
+
 
 if __name__ == '__main__':
     bot = Catalk(command_prefix=PREFIX)
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(bot.start())
+        loop.run_until_complete(bot.start(TOKEN))
     except KeyboardInterrupt:
         loop.run_until_complete(bot.logout())
     # cancel all tasks lingering
